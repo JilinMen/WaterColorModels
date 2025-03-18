e1ece87f3b3f87441f8d3ab3956bbeb36e028007
Jilin Men <1032764332@qq.com>
2025-03-18 13:29:19 -0500

    Update Chlorophyll-a.py

--- Chlorophyll-a.py
+++ Chlorophyll-a.py
@@ -1,44 +1,31 @@
-# chlorophyll-a concentration remote sensing algorithms
+# chlorophyll-a concentration remote sensing algorithms for Landsat 8/9
 # By jmen@ua.edu
 # 3/18/2025
 
 import numpy as np
 
-def chl_OC4(Rrs_443, Rrs_490, Rrs_510, Rrs_555):
+def chl_OC3(B1, B2, B3):
     """
-    OC4 algorithm for estimating chlorophyll-a concentration (O'Reilly et al., 1998)
     O'Reilly, J., Werdell, P., 2019. Chlorophyll algorithms for ocean color sensors - OC4, OC5
     & OC6. Remote Sens. Environ. 229, 32–47.
-    Inputs:
-    -------
-    Rrs_443 : float
-        Remote sensing reflectance at 443 nm in sr^-1
-    Rrs_490 : float
-        Remote sensing reflectance at 490 nm in sr^-1
-    Rrs_510 : float
-        Remote sensing reflectance at 510 nm in sr^-1
-    Rrs_555 : float
-        Remote sensing reflectance at 555 nm in sr^-1
-        
-    Output:
-    -------
-    chla : float
-        Estimated chlorophyll-a concentration in mg/m^3
+    B1-443
+    B2-482
+    B3-561
     """
     # chlulate maximum band ratio
-    numerator = max(Rrs_443, Rrs_490, Rrs_510)
-    denominator = Rrs_555
+    numerator = max(B1, B2)
+    denominator = B3
     MBR = numerator / denominator
-    
+
     # chlulate log10 of band ratio
     R = np.log10(MBR)
     
     # Coefficients
-    c0 = 0.3272
-    c1 = -2.9940
-    c2 = 2.7218
-    c3 = -1.2259
-    c4 = -0.5683
+    c0 = 0.30963
+    c1 = -2.40052
+    c2 = 1.28932
+    c3 = 0.52802
+    c4 = -1.33825
     
     # chlulate chlorophyll-a concentration
     log_chla = c0 + c1*R + c2*R**2 + c3*R**3 + c4*R**4
@@ -46,152 +33,28 @@ def chl_OC4(Rrs_443, Rrs_490, Rrs_510, Rrs_555):
     
     return chla
 
-def chl_OC3Mv6(Rrs_443, Rrs_490, Rrs_555):
-    """
-    OC3Mv6 algorithm for estimating chlorophyll-a concentration (Lewis et al., 2016)
-    Lewis, K., Mitchell, B., Dijken, V., Arrigo, K., 2016. Regional chlorophyll a algorithms in
-    the Arctic Ocean and their effect on satellite-derived primary production estimates.
-    Deep Sea Res. Part II 130, 14–27.
-    Inputs:
-    -------
-    Rrs_443 : float
-        Remote sensing reflectance at 443 nm in sr^-1
-    Rrs_490 : float
-        Remote sensing reflectance at 490 nm in sr^-1
-    Rrs_555 : float
-        Remote sensing reflectance at 555 nm in sr^-1
-        
-    Output:
-    -------
-    chla : float
-        Estimated chlorophyll-a concentration in mg/m^3
-    """
-    # chlulate maximum band ratio
-    numerator = max(Rrs_443, Rrs_490)
-    denominator = Rrs_555
-    MBR = numerator / denominator
-    
-    # chlulate log10 of band ratio
-    R = np.log10(MBR)
-    
-    # Coefficients
-    c0 = 0.2424
-    c1 = -2.5828
-    c2 = 1.7057
-    c3 = -0.3415
-    c4 = -0.8818
-    
-    # chlulate chlorophyll-a concentration
-    log_chla = c0 + c1*R + c2*R**2 + c3*R**3 + c4*R**4
-    chla = 10**log_chla
-    
-    return chla
-
-def chl_OCI_Wang(Rrs_443, Rrs_551, Rrs_671):
-    """
-    OCI-Wang algorithm for estimating chlorophyll-a concentration (Wang & Son, 2016)
-    Wang, M., Son, S., 2016. VIIRS-derived chlorophyll-a using the ocean color index method.
-    Remote Sens. Environ. 182, 141–149.
-    Inputs:
-    -------
-    Rrs_443 : float
-        Remote sensing reflectance at 443 nm in sr^-1
-    Rrs_551 : float
-        Remote sensing reflectance at 551 nm in sr^-1
-    Rrs_671 : float
-        Remote sensing reflectance at 671 nm in sr^-1
-        
-    Output:
-    -------
-    chla_a : float
-        Estimated chlorophyll-a concentration in mg/m^3
-    """
-    # Coefficients
-    c0 = -0.526
-    c1 = -0.474
-    c2 = 216.76
-    c3 = -0.4093
-    
-    # chlulate band ratio
-    r = Rrs_443 / Rrs_551
-    
-    # chlulate color index
-    CI = Rrs_551 + c0*Rrs_443 + c1*Rrs_671
-    
-    # chlulate chlorophyll-a concentration based on CI
-    if CI <= -0.0005:
-        chla_ci = 10**(c2+c3*CI)
-    else:
-        # For CI > -0.0005, use OC4 algorithm (simplified here)
-        chla_ci = chl_OC4(Rrs_443, Rrs_443, Rrs_443, Rrs_551)  # Approximation
-    
-    # chlulate OCx algorithm values (simplified to OC4)
-    chla_oc = chl_OC4(Rrs_443, Rrs_443, Rrs_443, Rrs_551)  # Approximation
-    
-    # Apply blending based on r value
-    if r > 4:
-        chla_a = chla_ci  # Use CI algorithm for r > 4
-    elif 2 < r <= 4:
-        w = 0.5 * (r - 2)  # Weight for blending
-        chla_a = w * chla_ci + (1 - w) * chla_oc  # Weighted average
-    else:  # r <= 2
-        chla_a = chla_oc  # Use OCx algorithm for r <= 2
-    
-    return chla_a
-
-def chl_OCI_Hu2011(Rrs_443, Rrs_555, Rrs_670, Rrs_490=None, Rrs_510=None):
+def chl_OCI_Hu2019(B1, B2, B3, B4):
     """
     OCI-Hu algorithm for estimating chlorophyll-a concentration (Hu et al., 2011)
     Hu, C., Lee, Z., Franz, B., 2011. Chlorophyll a algorithms for oligotrophic oceans: a novel
     approach based on three-band reflectance difference. J. Geophys. Res. Oceans 117
     (C1), C01011. https://doi.org/10.1029/2011JC007395.
-    Inputs:
-    -------
-    Rrs_443 : float
-        Remote sensing reflectance at 443 nm in sr^-1
-    Rrs_555 : float
-        Remote sensing reflectance at 555 nm in sr^-1
-    Rrs_670 : float
-        Remote sensing reflectance at 670 nm in sr^-1
-    Rrs_490 : float, optional
-        Remote sensing reflectance at 490 nm in sr^-1 (needed for OC4)
-    Rrs_510 : float, optional
-        Remote sensing reflectance at 510 nm in sr^-1 (needed for OC4)
-        
-    Output:
-    -------
-    Chl_oci : float
-        Estimated chlorophyll-a concentration in mg/m^3
     """
     # chlulate CI
-    CI = Rrs_555 - 0.5 * (Rrs_443 + Rrs_670)
+    CI = B3 - (B1 + 0.5 * (B4 -  B1))
+
+    Chl_ci = 10**(-0.4287 + 230.47 * CI)
+    Chl_oc3 = chl_OC3(B1, B2, B3)
     
-    # chlulate chlorophyll-a from CI
-    if CI <= -0.0005:
-        Chl_ci = 10**(-0.4909 + 191.6590 * CI)
-    else:
-        # For CI > -0.0005, use approximation
-        Chl_ci = 0.15  # Approximate value
+    Chl_L = 0.25
+    Chl_H = 0.4
+    Chl_hybird = Chl_oc3*(Chl_ci - Chl_L)/(Chl_H - Chl_L) + Chl_ci*(Chl_H - Chl_ci)/(Chl_H - Chl_L)
     
-    # chlulate OC4 algorithm if possible
-    if Rrs_490 is not None and Rrs_510 is not None:
-        Chl_oc4 = chl_OC4(Rrs_443, Rrs_490, Rrs_510, Rrs_555)
-    else:
-        # Use an approximation if full OC4 can't be chlulated
-        Chl_oc4 = 0.3  # Approximate value
-    
-    # Apply conditions
-    if Chl_ci <= 0.25:
-        Chl_oci = Chl_ci
-    elif Chl_ci > 0.3:
-        Chl_oci = Chl_oc4
-    else:  # 0.25 < Chl_ci <= 0.3
-        # Linear interpolation
-        alpha = (0.3 - Chl_ci) / 0.05
-        beta = (Chl_ci - 0.25) / 0.05
-        Chl_oci = alpha * Chl_oc4 + beta * Chl_ci
-    
-    return Chl_oci
+    Chl_final = Chl_hybird.copy()
+    Chl_final[Chl_ci <= Chl_L] = Chl_ci[Chl_ci <= Chl_L]
+    Chl_final[Chl_ci >= Chl_H] = Chl_oc3[Chl_ci >= Chl_H]    
+
+    return Chl_final
 
 def chl_Tassan(Rrs_412, Rrs_443, Rrs_490, Rrs_555):
     """
